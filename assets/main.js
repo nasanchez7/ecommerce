80c86cc8f272725d498966b21ff63ea756094730
nasanchez7 <nadirblanco02@gmail.com>
2022-07-27 12:34:51 -0300

    fetch

--- assets/main.js
+++ assets/main.js
@@ -23,47 +23,19 @@ guardarProductosCarrito(productosCarrito);
 
 let compraTotal = 0;
 
-//Constructor de productos
-class indumentaria{
-
-    constructor(id, marca, nombre, precio, imagen){
-        this.id = parseInt(id);
-        this.marca = marca;
-        this.nombre = nombre;
-        this.precio = parseInt(precio);
-        this.imagen = imagen;
-    }
-
-}
-
-//Productos
-const remeraAirMax = new indumentaria(1, "Nike", "Remera Air Max", 4500, "../assets/img/remera-airmax.png");
-const camperaNorthFace = new indumentaria(2, "North Face", "Campera North Face", 15000, "../assets/img/campera-northface.png");
-const zapatillasYeezy = new indumentaria(3, "Adidas", "Zapatillas Yeezy", 23000, "../assets/img/adidas-yeezy.png");
-const zapatillasSuperstar = new indumentaria(4, "Adidas", "Zapatillas SuperStar", 22000, "../assets/img/adidas-superstar.png");
-const zapatillasNewbalanceUno = new indumentaria(5, "New Balance", "Zapatillas New Balance", 25000, "../assets/img/newbalance-zapas.png");
-const zapatillasNewbalanceDos = new indumentaria(6, "New Balance", "Zapatillas New Balance", 20000, "../assets/img/newbalance-zapas2.png");
-const camperaAdidas = new indumentaria(7, "Adidas", "Campera Adidas retro", 14000, "../assets/img/campera-adidas.png");
-const zapatillasJordan = new indumentaria(8, "Jordan Nike", "Zapatillas Jordan", 32000, "../assets/img/jordan-nike.png");
-const zapatillasNike = new indumentaria(9, "Nike", "Zapatillas Nike", 19000, "../assets/img/nike-zapas.png");
-const buzoSupremeUno = new indumentaria(10, "Supreme", "Buzo Supreme", 20000, "../assets/img/supreme-buzo.png");
-const buzoSupremeDos = new indumentaria(11, "Supreme", "Buzo Supreme", 22500, "../assets/img/supreme-buzo2.png");
-const gorroSupreme = new indumentaria(12, "Supreme", "Gorro Supreme", 7500, "../assets/img/supreme-gorra.png");
-const conjuntoNike = new indumentaria(13, "Nike", "Conjunto Nike", 18000, "../assets/img/conjunto-nike.png");
-const ojotasAdidas = new indumentaria(14, "Adidas", "Ojotas Adidas", 5500, "../assets/img/ojotas-adidas.png");
-
-
-
-const productos = [remeraAirMax, camperaNorthFace, zapatillasYeezy, zapatillasSuperstar, zapatillasNewbalanceUno, 
-    zapatillasNewbalanceDos, camperaAdidas, zapatillasJordan, zapatillasNike, buzoSupremeUno, buzoSupremeDos, gorroSupreme,
-    conjuntoNike, ojotasAdidas];
-
-guardarProductos(productos);
-
-
 //Secciones HTML
 
 const seccionProductos = document.getElementById("productos");
+
+const seccionProductosNike = document.getElementById("productosNike");
+
+const seccionProductosAdidas= document.getElementById("productosAdidas");
+
+const seccionProductosSupreme = document.getElementById("productosSupreme");
+
+const seccionProductosNorthFace = document.getElementById("productosNorthFace");
+
+const seccionProductosNewBalance = document.getElementById("productosNewBalance");
 
 const contadorCarrito = document.getElementById("contadorCarrito");
 
@@ -89,9 +61,15 @@ cerrarCarrito.onclick = () => {
 
 //Renderizado de productos
 
-function renderProductos(){
-    obtenerProductos();
-    for(const producto of productos){
+async function renderProductos(){
+    
+    const infoProductos = await fetch("../assets/productos.json");
+    const productos = await infoProductos.json();
+    guardarProductos(productos);
+    const arrayProductos = obtenerProductos();
+    const productosDestacados = arrayProductos.slice(0, 5);
+
+    for(const producto of productosDestacados){
         const tarjetas = document.createElement("div");
         tarjetas.className = "tarjeta";
         tarjetas.innerHTML = `  <img src="${producto.imagen}" alt="${producto.nombre}">
@@ -101,19 +79,126 @@ function renderProductos(){
                                 <a href="">Ver mas detalles</a>
                                 `
         seccionProductos.appendChild(tarjetas);
-    }    
+    }
+}
+
+async function renderProductosNike(){
+    
+    const infoProductos = await fetch("../assets/productos.json");
+    const productos = await infoProductos.json();
+    guardarProductos(productos);
+    const arrayProductos = obtenerProductos();
+    const productosNike = arrayProductos.filter((item => item.marca == "Nike"));
+    
+    for(const producto of productosNike){
+        const tarjetas = document.createElement("div");
+        tarjetas.className = "tarjeta";
+        tarjetas.innerHTML = `  <img src="${producto.imagen}" alt="${producto.nombre}">
+                                <h3>${producto.nombre}</h3>
+                                <h4>$ ${producto.precio}</h4>
+                                <button class="boton1" onclick="agregarCarrito(${producto.id})" >Añadir al carrito</button>
+                                <a href="">Ver mas detalles</a>
+                                `
+        seccionProductosNike.appendChild(tarjetas);
+    }
+}
+
+async function renderProductosAdidas(){
+    
+    const infoProductos = await fetch("../assets/productos.json");
+    const productos = await infoProductos.json();
+    guardarProductos(productos);
+    const arrayProductos = obtenerProductos();
+    const productosAdidas = arrayProductos.filter((item => item.marca == "Adidas"));
+    
+    for(const producto of productosAdidas){
+        const tarjetas = document.createElement("div");
+        tarjetas.className = "tarjeta";
+        tarjetas.innerHTML = `  <img src="${producto.imagen}" alt="${producto.nombre}">
+                                <h3>${producto.nombre}</h3>
+                                <h4>$ ${producto.precio}</h4>
+                                <button class="boton1" onclick="agregarCarrito(${producto.id})" >Añadir al carrito</button>
+                                <a href="">Ver mas detalles</a>
+                                `
+        seccionProductosAdidas.appendChild(tarjetas);
+    }
+}
+
+async function renderProductosSupreme(){
+    
+    const infoProductos = await fetch("../assets/productos.json");
+    const productos = await infoProductos.json();
+    guardarProductos(productos);
+    const arrayProductos = obtenerProductos();
+    const productosSupreme = arrayProductos.filter((item => item.marca == "Supreme"));
+    
+    for(const producto of productosSupreme){
+        const tarjetas = document.createElement("div");
+        tarjetas.className = "tarjeta";
+        tarjetas.innerHTML = `  <img src="${producto.imagen}" alt="${producto.nombre}">
+                                <h3>${producto.nombre}</h3>
+                                <h4>$ ${producto.precio}</h4>
+                                <button class="boton1" onclick="agregarCarrito(${producto.id})" >Añadir al carrito</button>
+                                <a href="">Ver mas detalles</a>
+                                `
+        seccionProductosSupreme.appendChild(tarjetas);
+    }
+}
+
+async function renderProductosNorthFace(){
+    
+    const infoProductos = await fetch("../assets/productos.json");
+    const productos = await infoProductos.json();
+    guardarProductos(productos);
+    const arrayProductos = obtenerProductos();
+    const productosNorthFace = arrayProductos.filter((item => item.marca == "North Face"));
+    
+    for(const producto of productosNorthFace){
+        const tarjetas = document.createElement("div");
+        tarjetas.className = "tarjeta";
+        tarjetas.innerHTML = `  <img src="${producto.imagen}" alt="${producto.nombre}">
+                                <h3>${producto.nombre}</h3>
+                                <h4>$ ${producto.precio}</h4>
+                                <button class="boton1" onclick="agregarCarrito(${producto.id})" >Añadir al carrito</button>
+                                <a href="">Ver mas detalles</a>
+                                `
+        seccionProductosNorthFace.appendChild(tarjetas);
+    }
+}
+
+async function renderProductosNewBalance(){
+    
+    const infoProductos = await fetch("../assets/productos.json");
+    const productos = await infoProductos.json();
+    guardarProductos(productos);
+    const arrayProductos = obtenerProductos();
+    const productosNewBalance = arrayProductos.filter((item => item.marca == "New Balance"));
+    
+    for(const producto of productosNewBalance){
+        const tarjetas = document.createElement("div");
+        tarjetas.className = "tarjeta";
+        tarjetas.innerHTML = `  <img src="${producto.imagen}" alt="${producto.nombre}">
+                                <h3>${producto.nombre}</h3>
+                                <h4>$ ${producto.precio}</h4>
+                                <button class="boton1" onclick="agregarCarrito(${producto.id})" >Añadir al carrito</button>
+                                <a href="">Ver mas detalles</a>
+                                `
+        seccionProductosNewBalance.appendChild(tarjetas);
+    }
 }
 
 //Elemento seleccionado
 
 function seleccionado (id){
+    const productos = obtenerProductos();
     return productos.find(x => x.id == id);
 }
-
 
 //Agregar elemento al carrito
 
 function agregarCarrito(id){
+
+    
     let carritoProductos = obtenerProductosCarrito();
     let posicion = carritoProductos.findIndex(x => x.id == id);
 
@@ -122,6 +207,7 @@ function agregarCarrito(id){
         compraTotal = compraTotal + carritoProductos[posicion].precio;
     }else{
         let productoSeleccionado = seleccionado(id);
+        console.log(productoSeleccionado);
         productoSeleccionado.cantidad = 1;
         compraTotal = compraTotal + productoSeleccionado.precio;
         carritoProductos.push(productoSeleccionado);
@@ -129,6 +215,7 @@ function agregarCarrito(id){
 
     guardarProductosCarrito(carritoProductos);
 
+    
     productosEnCarrito.innerHTML = "";
 
     for(let producto of carritoProductos){
@@ -177,8 +264,6 @@ function agregarCarrito(id){
 
 function eliminarCarrito(id){
     let carritoProductos = obtenerProductosCarrito();
-    let productoSeleccionado = seleccionado(id);
-    console.log(productoSeleccionado);
     let posicion = carritoProductos.findIndex(x => x.id == id);
     carritoProductos[posicion].cantidad -= 1;
     compraTotal = 0;
@@ -330,4 +415,8 @@ function confirmarCompra1(){
 
 //Renderizado de productos
 renderProductos();
-
+renderProductosNike();
+renderProductosAdidas();
+renderProductosSupreme();
+renderProductosNorthFace();
+renderProductosNewBalance();
